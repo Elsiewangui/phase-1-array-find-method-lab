d27ab92fff971937f0416475654fccac09ee1262
Wangui <elswangui@gmail.com>
2024-07-01 10:58:39 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+    const superBowl = record.find(record =>record.result ==="W")
+
+    if (superBowl){
+        return superBowl.year;
+    } else{
+        return undefined
+    }
+
+}
+console.log(superbowlWin(record))
